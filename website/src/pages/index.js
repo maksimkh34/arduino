c336f9f75a157e7d50289ef45fb4ad93b46047fa
maksimkh@lenovo <maksimkh34@duck.com>
2026-06-03 17:14:21 +0300

    fix: Add redirect to first chapter and fix GH Actions node deprecation

---
+++ website/src/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Redirect} from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+export default function Home() {
+  return <Redirect to={useBaseUrl('/01-01-arduino-mega-first-contact')} />;
+}
